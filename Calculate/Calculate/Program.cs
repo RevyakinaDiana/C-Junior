f47dddd2106ba57537fe8ab0177e58fbf03b6957
RevyakinaDiana <r.d.a.1997@yandex.ru>
2018-05-14 15:30:15 +0300

    Two commit

--- Calculate/Calculate/Program.cs
+++ Calculate/Calculate/Program.cs
@@ -10,6 +10,15 @@ namespace Calculate
     {
         static void Main(string[] args)
         {
+
+            if (args.Length !=2 )
+            {
+                Console.WriteLine("Введите 2");
+                return;
+            }
+            Console.WriteLine(Convert.ToInt64(args[0] + Convert.ToInt64(args[0])));
+            Console.ReadKey();
+
         }
     }
 }
