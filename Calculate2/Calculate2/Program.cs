1a3270b7db8b63b518181a710c582cf82d6c9775
RevyakinaDiana <r.d.a.1997@yandex.ru>
2018-05-14 22:07:24 +0300

    Commit

---
+++ Calculate2/Calculate2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate2
+{
+    class Program
+    {
+        static int a; static int b;
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Выберите операцию - Sum, Min, Pow, Mult, Div");
+          string result= Console.ReadLine();
+            if(result =="Sum")
+                {
+                Metods.summa(a, b);            
+                 }
+          
+            if(result=="Min")
+            {
+                Metods.min(a, b);
+            }
+            if(result=="Mult")
+            {
+                Metods.mult(a, b);
+            }
+            if(result=="Pow")
+            {
+                Metods.pow(a, b);
+            }
+            if(result=="Div")
+
+            {
+                Metods.div(a, b);
+            }
+        }
+    }
+}
---
+++ Calculate2/Calculate2/Sum.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate2
+{
+    class Metods
+    {
+        public static void summa(int a, int b)
+        {
+            Console.WriteLine("Введите 2 числа");
+            a = Convert.ToInt32(Console.ReadLine());
+            b= Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("Сумма: " + c);
+            Console.ReadKey();
+        }
+        public static void min(int a, int b)
+        {
+            Console.WriteLine("Введите 2 числа");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            int c = a - b;
+            Console.WriteLine("Разность: " + c);
+            Console.ReadKey();
+        }
+        public static void mult(int a, int b)
+        {
+            Console.WriteLine("Введите 2 числа");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            int c = a * b;
+            Console.WriteLine("Произведение: " + c);
+            Console.ReadKey();
+        }
+        public static void pow(int a, int b)
+        {
+            Console.WriteLine("Введите 2 числа");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Math.Pow(a, b));
+            Console.WriteLine("А в степени В: " + c);
+            Console.ReadKey();
+        }
+        public static void div(int a,int b)
+        {
+            Console.WriteLine("Введите 2 числа");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            double c = a / b;
+                Console.WriteLine("Частное: " + c);
+            Console.ReadKey();
+        }
+    }
+}
